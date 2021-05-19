06e37537c2192035484d6a6e959553c3eba8ab6f
Matheus Casagrande <matheuscasagrand@gmail.com>
2021-05-19 15:10:23 -0300

    alterações simples

--- src/entities/OrderItem.java
+++ src/entities/OrderItem.java
@@ -12,4 +12,20 @@ public class OrderItem {
 		this.price = price;
 	}
 
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
 }
